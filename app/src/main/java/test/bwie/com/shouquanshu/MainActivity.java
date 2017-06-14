d29adecfc3055fd42d66e5440283d050a3784895
GaoJianYi <958335970@qq.com>
2017-06-14 19:49:17 +0800

    短信Mob

--- app/src/main/java/test/bwie/com/shouquanshu/MainActivity.java
+++ app/src/main/java/test/bwie/com/shouquanshu/MainActivity.java
@@ -3,12 +3,14 @@ package test.bwie.com.shouquanshu;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import cn.smssdk.SMSSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SMSSDK.initSDK(this, "1cfc8f67e3720", "dbe82e103eb31bc9528f0fbfc33ff221");
     }
 }
---
+++ app/src/main/java/test/bwie/com/shouquanshu/MyApp.java
@@ -0,0 +1,16 @@
+package test.bwie.com.shouquanshu;
+
+import android.app.Application;
+
+/**
+ * date:2017/6/14
+ * author:高坚译（dell）
+ * Time:19:36
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
